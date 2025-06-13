e884900562c543ffcccc94a2001d8116e1e30135
Askari <askarir64@gmail.com>
2025-06-13 13:28:32 +0530

    footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,13 @@ export default function Home() {
   const CopySanitizedUrl = async () => {
     if (!sanitizedUrl) return;
     await navigator.clipboard.writeText(sanitizedUrl);
-    toast("Link has been copied to clipboard")
+    toast("Link has been copied to clipboard");
   };
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      
       <main className="flex flex-col gap-[32px] row-start-2 items-center">
-        <AnimatedHeading/>
+        <AnimatedHeading />
         <InputWithButton onSanitize={handleSanitizedUrl} />
         {sanitizedUrl && (
           <>
@@ -38,6 +37,15 @@ export default function Home() {
           </>
         )}
       </main>
+      <footer className="row-start-3 w-full text-center py-4 mt-8 border-t border-gray-200 text-sm text-muted-foreground flex items-center justify-center gap-2">
+        <p>
+          Made with{" "}
+          <span className="text-red-500" role="img" aria-label="love">
+            ❤️
+          </span>{" "}
+          by <a href="https://askaririzvi.site/" target="_blank" rel="noopener noreferrer" className=" text-gray-500 hover:underline font-semibold mx-1">Askari</a>
+        </p>
+      </footer>
     </div>
   );
 }
