916d56ec8c1fd54e5c26a2aa6eb2bac6f41cb856
Askari <askarir64@gmail.com>
2025-06-13 11:23:02 +0530

    added toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 "use client";
+
 import { useState } from "react";
 import { InputWithButton } from "@/components/derivedComponents/InputWithButton";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 import { sanitizer } from "@/lib/utils";
 
 export default function Home() {
@@ -11,14 +14,25 @@ export default function Home() {
     SetSanitizedUrl(result);
   };
 
+  const CopySanitizedUrl = async () => {
+    if (!sanitizedUrl) return;
+    await navigator.clipboard.writeText(sanitizedUrl);
+    toast("Link has been copied to clipboard")
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
         <InputWithButton onSanitize={handleSanitizedUrl} />
         {sanitizedUrl && (
-          <p className="text-sm text-muted-foreground break-all">
-            Sanitized URL: {sanitizedUrl}
-          </p>
+          <>
+            <p className="text-sm text-muted-foreground break-all">
+              Sanitized URL: {sanitizedUrl}
+            </p>
+            <Button variant="outline" onClick={CopySanitizedUrl}>
+              Copy Link
+            </Button>
+          </>
         )}
       </main>
     </div>
