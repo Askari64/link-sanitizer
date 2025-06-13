c97de564e84e590baac48bf8666fe1d7b8e0c736
Askari <askarir64@gmail.com>
2025-06-13 13:02:00 +0530

    rule change

--- src/components/ui/container-text-flip.tsx
+++ src/components/ui/container-text-flip.tsx
@@ -33,6 +33,7 @@ export function ContainerTextFlip({
   const updateWidthForWord = () => {
     if (textRef.current) {
       // Add some padding to the text width (30px on each side)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       const textWidth = textRef.current.scrollWidth + 30;
       setWidth(textWidth);
