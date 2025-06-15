eeffe6996d73a07a2829d19f38c594f078b51621
Askari <askarir64@gmail.com>
2025-06-15 19:50:56 +0530

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import { InputWithButton } from "@/components/derivedComponents/InputWithButton";
-import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { sanitizer } from "@/lib/utils";
 import { AnimatedHeading } from "@/components/AnimatedHeading";
@@ -10,14 +9,10 @@ import { AnimatedHeading } from "@/components/AnimatedHeading";
 export default function Home() {
   const [sanitizedUrl, SetSanitizedUrl] = useState("");
 
-  const handleSanitizedUrl = (url: string) => {
+  const handleSanitizedUrl = async (url: string) => {
     const result = sanitizer(url);
     SetSanitizedUrl(result);
-  };
-
-  const CopySanitizedUrl = async () => {
-    if (!sanitizedUrl) return;
-    await navigator.clipboard.writeText(sanitizedUrl);
+    await navigator.clipboard.writeText(result);
     toast("Link has been copied to clipboard");
   };
 
@@ -27,14 +22,9 @@ export default function Home() {
         <AnimatedHeading />
         <InputWithButton onSanitize={handleSanitizedUrl} />
         {sanitizedUrl && (
-          <>
             <p className="text-sm text-muted-foreground break-all">
               Sanitized URL: {sanitizedUrl}
             </p>
-            <Button variant="outline" onClick={CopySanitizedUrl}>
-              Copy Link
-            </Button>
-          </>
         )}
       </main>
       <footer className="row-start-3 w-full text-center py-4 mt-8 border-t border-gray-200 text-sm text-muted-foreground flex items-center justify-center gap-2">
@@ -43,7 +33,15 @@ export default function Home() {
           <span className="text-red-500" role="img" aria-label="love">
             ❤️
           </span>{" "}
-          by <a href="https://askaririzvi.site/" target="_blank" rel="noopener noreferrer" className=" text-gray-500 hover:underline font-semibold mx-1">Askari</a>
+          by{" "}
+          <a
+            href="https://askaririzvi.site/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" text-gray-500 hover:underline font-semibold mx-1"
+          >
+            Askari
+          </a>
         </p>
       </footer>
     </div>
